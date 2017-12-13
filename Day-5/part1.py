c7f0c618572bb5c04018add7649915ad1089b512
Yarince Martis <yarince.martis@silverstreet.com>
2017-12-13 22:03:57 +0800

    Day 5 part 2

---
+++ Day-5/part1.py
@@ -0,0 +1,22 @@
+import time
+with open("input.csv") as f:
+    content = f.readlines()
+jumps = [int(row.strip()) for row in content]
+
+start_time = time.time()
+
+steps = 0
+i = 0
+while i <= len(jumps):
+    old_i = i
+
+    if jumps[i] != 0:
+        i += jumps[i]
+
+    jumps[old_i] += 1
+    steps += 1
+    if i >= len(jumps):
+        break
+
+print("Ammount of steps: ", steps)
+print("Lol it took", time.time() - start_time, " seconds to run")
---
+++ Day-5/part2.py
@@ -0,0 +1,28 @@
+import time
+with open("input.csv") as f:
+    content = f.readlines()
+jumps = [int(row.strip()) for row in content]
+# jumps = [0, 3, 0, 1, -3]
+
+start_time = time.time()
+
+steps = 0
+i = 0
+while i <= len(jumps):
+    old_i = i
+
+    if jumps[i] != 0:
+        i += jumps[i]
+
+    if jumps[old_i] >= 3:
+        jumps[old_i] -= 1
+    else:
+        jumps[old_i] += 1
+
+    steps += 1
+
+    if i >= len(jumps):
+        break
+
+print("Ammount of steps: ", steps)
+print("Lol it took", time.time() - start_time, " seconds to run")
